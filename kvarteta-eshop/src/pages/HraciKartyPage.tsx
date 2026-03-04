0b91260375480b5e7120b99f51873c35afea5068
karelhromada <karel.hromada30@gmail.com>
2026-03-05 00:02:44 +0100

    fix: zakomentování importu KartyCreator k vyřešení warningu

--- kvarteta-eshop/src/pages/HraciKartyPage.tsx
+++ kvarteta-eshop/src/pages/HraciKartyPage.tsx
@@ -3,7 +3,7 @@ import type { CartItem } from '../App';
 import HeroSectionKarty from '../components/HeroSectionKarty';
 import FeaturesSectionKarty from '../components/FeaturesSectionKarty';
 import ProductShowcaseKarty from '../components/ProductShowcaseKarty';
-import KartyCreator from '../components/KartyCreator';
+// import KartyCreator from '../components/KartyCreator';
 
 interface HraciKartyPageProps {
     onAddToCart: (product: Omit<CartItem, 'quantity'>) => void;
